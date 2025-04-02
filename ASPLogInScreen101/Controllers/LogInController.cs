3fd5bd492e67a00384e435ebc34db811f4f76c76
bumin <bumintemp@gmail.com>
2025-04-02 13:27:46 +0300

    first commit

---
+++ ASPLogInScreen101/Controllers/LogInController.cs
@@ -0,0 +1,30 @@
+﻿using ASPLogInScreen101.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPLogInScreen101.Controllers
+{
+    public class LogInController : Controller
+    {
+        // GET: LogIn
+        public ActionResult Index()
+        {
+            return View("LogInView");
+        }
+
+        [HttpPost]
+        public ActionResult Login(LogInModel model)
+        {
+            if (model.Username == "admin" && model.Password == "admin")
+            {
+                return RedirectToAction("Index", "Main");
+            }
+
+            ViewBag.Error = "Geçersiz kullanıcı adı veya şifre!";
+            return View("LogInView");
+        }
+    }
+}
---
+++ ASPLogInScreen101/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPLogInScreen101.MvcApplication" Language="C#" %>
